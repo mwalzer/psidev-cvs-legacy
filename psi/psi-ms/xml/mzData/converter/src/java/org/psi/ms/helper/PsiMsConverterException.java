6f29bcb8a662e8e2d3c51494b660e08f4f30285a
krunte <>
2003-08-11 17:22:16 +0000

    Initial checkin.

---
+++ psi/psi-ms/xml/mzData/converter/src/java/org/psi/ms/helper/PsiMsConverterException.java
@@ -0,0 +1,20 @@
+/**
+ * $Id$
+ *
+ * Created by IntelliJ IDEA.
+ * User: krunte
+ * Date: Jul 23, 2003
+ * Time: 5:45:33 PM
+ */
+package org.psi.ms.helper;
+
+/**
+ * 
+ * @author krunte
+ */
+public class PsiMsConverterException extends Exception {
+
+    public PsiMsConverterException(String message) {
+        super(message);
+    }
+}
