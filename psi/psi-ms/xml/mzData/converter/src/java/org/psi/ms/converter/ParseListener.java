3fe588038d18983bfd62a6ea127db410e681b6fd
krunte <>
2003-09-11 15:38:59 +0000

    Lawrence added a ParseListener interface for communication with the GUI he's written.

---
+++ psi/psi-ms/xml/mzData/converter/src/java/org/psi/ms/converter/ParseListener.java
@@ -0,0 +1,22 @@
+/**
+ * $Id$
+ *
+ * Created by IntelliJ IDEA.
+ * User: lbower
+ * Date: Sep 11, 2003
+ * Time: 3:04:48 PM
+ */
+package org.psi.ms.converter;
+
+/**
+ * This interface is used for communication between Converter and GUI.
+ *
+ * @author lbower
+ */
+public interface ParseListener {
+    public void fileParsed();
+
+    public void indeterminiteProcess();
+
+    public void setMessage(String message);
+}
